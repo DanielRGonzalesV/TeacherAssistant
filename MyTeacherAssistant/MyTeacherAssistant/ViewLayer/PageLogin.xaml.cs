9b6b309b48fdc1a7f4ec26f503f8bfa5403445ed
Daniel Gonzales <drgvandroid@gmail.com>
2018-03-16 17:55:59 -0400

    - Añadir funcionalidad Login button

---
+++ MyTeacherAssistant/MyTeacherAssistant/ViewLayer/PageLogin.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MyTeacherAssistant.ViewLayer
+{
+    /// <summary>
+    /// Interaction logic for PageLogin.xaml
+    /// </summary>
+    public partial class PageLogin : Page
+    {
+        public PageLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, RoutedEventArgs e)
+        {
+
+            // Instantiate the page to navigate to
+            PagePrincipalAlumno page = new PagePrincipalAlumno();
+
+            // Navigate to the page, using the NavigationService
+            this.NavigationService.Navigate(page);
+
+            //// Force WPF to download this page again
+            //this.NavigationService.Refresh();
+        }
+
+        private void cerrarBtn_Click(object sender, RoutedEventArgs e)
+        {
+            App.Current.Shutdown();
+        }
+    }
+}
