60b7929fa76bfbd45c9bc638b1ec3118a212d221
DanielG <drgvandroid@gmail.com>
2018-03-16 21:41:55 -0400

    - Base de datos y page princiapl

---
+++ MyTeacherAssistant/MyTeacherAssistant/BussinesLayer/Validaciones.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MyTeacherAssistant.BussinesLayer
+{
+    class Validaciones
+    {
+        internal bool validarLogin(TextBox usernameTbx, PasswordBox passwordBox)
+        {
+            DataLayer.EntidadDocente docente = new DataLayer.EntidadDocente();
+            return docente.getLogin(usernameTbx, passwordBox);
+            throw new NotImplementedException();
+
+        }
+    }
+}
